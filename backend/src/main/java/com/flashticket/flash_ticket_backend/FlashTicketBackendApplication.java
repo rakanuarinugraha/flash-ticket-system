4e2178b4a113b85e08b58f844c3f61808cb7ccaa
Raka Nugraha <rakanuarinugraha@gmail.com>
2026-09-17 20:46:36 +0700

    feat(backend): initialize spring boot 3 project with postgresql jpa config

---
+++ backend/src/main/java/com/flashticket/flash_ticket_backend/FlashTicketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flashticket.flash_ticket_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashTicketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashTicketBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/flashticket/flash_ticket_backend/FlashTicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashticket.flash_ticket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashTicketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
